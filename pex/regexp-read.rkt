b42fa8e7e593c585880642d15cc7d70acab7b786
Jan Dvořák <mordae@anilinux.org>
2015-10-06 13:23:53 +0200

    Fix the regexp-based-reading utility
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- pex/regexp-read.rkt
+++ pex/regexp-read.rkt
@@ -10,12 +10,14 @@
 
 
 (define (regexp-match-bytes-evt b-rx in)
-  (match (regexp-match-evt b-rx in)
-    ((list-rest head _)
-     (values head))
+  (wrap-evt (regexp-match-evt b-rx in)
+            (λ (result)
+              (match result
+                ((list-rest head _)
+                 (values head))
 
-    (eof-or-false
-     (values eof-or-false))))
+                (eof-or-false
+                  (values eof-or-false))))))
 
 
 ; vim:set ts=2 sw=2 et:
